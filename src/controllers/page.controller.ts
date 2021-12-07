1cc56fa20866bd0e6600430e56a3a2db2b37741c
Guilherme Fontanella <guilherme.fontanella@squadra.com.br>
2021-12-07 00:49:28 -0300

    separando as views

--- src/controllers/page.controller.ts
+++ src/controllers/page.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-    res.send('Página Principal')
+    res.render('pages/page')
 }
 
 export const dogs = (req: Request, res: Response) => {
